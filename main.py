b2f1cdcf1cf81fd0afb1cf564ad62babc5b09623
Mai allam <maimallam57@gmail.com>
2023-10-13 13:52:14 +0300

    test2

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from calculator.addition_package import __main__
 from calculator.multiplication_package import __main__
 
 
-print("main of addition_package")
-print("main of multiplication_package")
+print("hello from addition_package")
+
+print("hello from multiplication_package")
